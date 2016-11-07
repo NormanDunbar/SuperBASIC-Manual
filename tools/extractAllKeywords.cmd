70bd08871c309addf1ff7d7ce7e748894ef01c40
NormanDunbar <norman@dunbar-it.co.uk>
2016-11-07 13:18:50 +0000

    Updates to the keyword extraction batch file.

--- tools/extractAllKeywords.cmd
+++ tools/extractAllKeywords.cmd
@@ -1,5 +1,6 @@
-REM Because the shell is brain dead, or appears so, I need
-REM to pass each file individually. It cannot cope with a
-REM wildcard list of files such as Keywords*.clean.rst. Sigh.
-REM
+@echo off
+@REM Because the shell is brain dead, or appears so, I need
+@REM to pass each file individually. It cannot cope with a
+@REM wildcard list of files such as Keywords*.clean.rst. Sigh.
+@REM
 for %%f in (Keywords*.clean.rst) do @extractKeywords %%f
